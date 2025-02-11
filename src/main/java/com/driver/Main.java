84643053c5f2ffe2874b1e0b38972b4399000fc1
Sathwik-Goloju <sathwik.goloju@gmail.com>
2025-02-11 20:00:56 +0530

    Understand Polymorphism Update Main.java
    Understand Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-
+  Product p= new Product();
+  p.product(x,y);
+  p.product(x,y,z);
+ p.product(x,y);
+public class Product{
+  public int product(int x,int y){}
+  public product(int x,int y,int z){}
+  public double product(double x, double y) {}
+}
 }
