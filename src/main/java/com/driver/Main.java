5ea52481467a3a9beff901c162bde2c21b270191
Sathwik-Goloju <sathwik.goloju@gmail.com>
2025-02-14 19:40:59 +0530

    updated commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,23 +6,22 @@ package com.driver;
 
 public class Main {
     public Main() {
-        Product p = new Product();
-        p.product(0,0);
-        p.product(0,0,0);
-        p.product(0.0,0.0);
     }
 
     public static void main(java.lang.String[] args) {
-
+        Product p = new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.0,2.0);
     }
 
     public static class Product {
         public Product() {  }
 
-        public int product(int x, int y) { return 0; }
+        public int product(int x, int y) { return x*y; }
 
-        public int product(int x, int y, int z) {  return 0;}
+        public int product(int x, int y, int z) {  return x*y*z;}
 
-        public double product(double x, double y) { return 0.0;}
+        public double product(double x, double y) { return x*y;}
     }
 }
