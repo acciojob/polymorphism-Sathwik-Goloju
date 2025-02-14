22232fbfcaafba5c04d9231a3f7b64848d900166
Sathwik-Goloju <sathwik.goloju@gmail.com>
2025-02-14 18:55:47 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,13 +1,4 @@
 package com.driver;
 
 public class Main {
-  Product p= new Product();
-  p.product(x,y);
-  p.product(x,y,z);
- p.product(x,y);
-public class Product{
-  public int product(int x,int y){}
-  public product(int x,int y,int z){}
-  public double product(double x, double y) {}
-}
 }
