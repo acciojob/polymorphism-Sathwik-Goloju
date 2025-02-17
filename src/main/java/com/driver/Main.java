42a59750a9267b9de5536fab03d523077636ea41
Sathwik-Goloju <sathwik.goloju@gmail.com>
2025-02-17 19:36:32 +0530

    updated code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,6 +12,7 @@ public class Main {
         System.out.println( p.product(1,2));
         System.out.println(p.product(1,2,3));
         System.out.println(p.product(1.0,2.0));
+
     }
 
     public static class Product {
